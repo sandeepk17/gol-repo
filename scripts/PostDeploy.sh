654e42bd5cda14bda2aa54a3996672220c379099
Rasmiramachandran <rasmimr91@gmail.com>
2020-04-12 09:21:13 +0200

    Added changes to the directory

--- scripts/PostDeploy.sh
+++ scripts/PostDeploy.sh
@@ -3,8 +3,10 @@ export Tuttu_env=`get_octopusvariable "Tuttu_env"`
 export Tuttu_machine=`get_octopusvariable "Tuttu_machine"`
 export Tuttu_deployed=`get_octopusvariable "Tuttu_deployed"`
 export Tuttu_version=`get_octopusvariable "Tuttu_version"`
+echo "######### Variable as Environmental VAriable ###########"
 echo "Tuttu Env  is---------------->: $Tuttu_env"
 echo "Tuttu Env  is---------------->: $Tuttu_machine"
+echo "############## RELEASE VARIABLES #######################"
 Releaseid=$(get_octopusvariable "Octopus.Release.Id")
 Tuttumachine=$(get_octopusvariable "Octopus.Tuttu_machine")
 Releaseno=$(get_octopusvariable "Octopus.Release.Number")
@@ -13,7 +15,8 @@ ReleasePackage=$(get_octopusvariable "Octopus.Release.Package")
 ReleaseDeploymentid=$(get_octopusvariable "Octopus.Deployment.Id")
 ReleaseEnvName=$(get_octopusvariable "Octopus.Environment.Name")
 ReleaseProjectid=$(get_octopusvariable "Octopus.Project.Id")
-echo "Releaseid is------------------>: $Releaseid"
+echo "######################################################"
+echo "Releaseid is==================>: $Releaseid"
 echo "Tuttu Machine is-------------->: $Tuttumachine"
 echo "Release no is----------------->: $Releaseno"
 echo "Release created is------------>: $Releasecreated"
@@ -21,33 +24,19 @@ echo "Release package is------------>: $ReleasePackage"
 echo "Release Deployment id is------>: $ReleaseDeploymentid"
 echo "Release Environmental name is->: $ReleaseEnvName"
 echo "Release Project ID is--------->: $ReleaseProjectid"
-#####################################################################
-echo "This is Post deploy test"
-# in pre-deploy, in post-deploy if custom installation directory has not been defined
-extractPath="$(get_octopusvariable "Octopus.Action.Package[dev package].CustomInstallationDirectory")"
-echo "This is not needed for the testscript ----------------> : $extractPath"
+echo "#######################################################"
+echo "<=============This is Post deploy SCRIPT ================>"
+
 # if a custom installation directory  where the contents of the package has been extracted
 customPath="$(get_octopusvariable "Octopus.Action.Output.Package[dev package].InstallationDirectoryPath")"
-echo "This is not needed for the testscript -----------------> : $customPath"
-
-# The app we built follows "12-factor" (https://12factor.net/), and gets its 
-# configuration from environment variables. The variables we set in this session
-# are captured by PM2 and stored for the application (so even if the app is 
-# restarted in a different session, it uses these variables)
+echo "This is not needed for the testscript =================>: $customPath"
 
 # Change to the directory where our Node.js app was extracted
 cd `get_octopusvariable "Octopus.Action[dev package].Output.Package.InstallationDirectoryPath"`
-echo "##Present working directory is-------------------------------------------------->"
+echo "##Present working directory is =================================>"
 pwd
 ls -l 
 
-# Start PM2
-# PM2 instance name: Phoenix_Tenant
 InstanceName="Phoenix_#{Tuttu_env}_#{Tuttu_machine}"
 
-echo "##octopus instance name --------------------------------------->: $InstanceName"
-#pm2 stop "$InstanceName"
-#pm2 delete "$InstanceName"
-
-#echo "##octopus[stderr-default]"
-#pm2 start ./bin/www --name "$InstanceName"
+echo "##octopus instance name ===============================>: $InstanceName"
--- scripts/PreDeploy.sh
+++ scripts/PreDeploy.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 ls -al
-echo "This is Predeploy test of DEV ENVIRONMENT "
-echo "This is not needed for the testscript ----> : $1"
+echo "This is Predeploy test of PreDeploy ENVIRONMENT SCRIPT "
 # in pre-deploy, in post-deploy if custom installation directory has not been defined
 extractPath="$(get_octopusvariable "Octopus.Action.Package.InstallationDirectoryPath")"
-echo "This is not needed for the testscript ----> : $extractPath"
+echo "This is extract path ---------------> : $extractPath"
 # if a custom installation directory has been defined
 customPath="$(get_octopusvariable "Octopus.Action.Package.CustomInstallationDirectory")"
-echo "This is not needed for the testscript ----> : $customPath"
+echo "This is custom path -----------------> : $customPath"
+# in pre-deploy, in post-deploy if custom installation directory has not been defined
+extractPath1="$(get_octopusvariable "Octopus.Action.Package[dev package].CustomInstallationDirectory")"
+echo "This is extract path1 ----------------> : $extractPath1"
