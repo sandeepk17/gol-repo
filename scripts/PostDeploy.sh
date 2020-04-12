6db35daccbd3868d2d6c454e931769667ebe021e
Rasmiramachandran <rasmimr91@gmail.com>
2020-04-12 07:11:52 +0200

    Added changes to deploy and predeploy

--- scripts/PostDeploy.sh
+++ scripts/PostDeploy.sh
@@ -13,4 +13,15 @@ echo "Connection string is: $ReleasePackage"
 echo "Connection string is: $ReleaseDeploymentid"
 echo "Connection string is: $ReleaseEnvName"
 echo "Connection string is: $ReleaseProjectid"
+#####################################################################
 echo "This is Post deploy test"
+echo "This is not needed for the testscript ----> : $1"
+# in pre-deploy, in post-deploy if custom installation directory has not been defined
+extractPath="$(get_octopusvariable "Octopus.Action.Package.CustomInstallationDirectory")"
+echo "This is not needed for the testscript ----> : $extractPath"
+# if a custom installation directory has been defined
+customPath="$(get_octopusvariable "Octopus.Action.Output.Package.InstallationDirectoryPath")"
+echo "This is not needed for the testscript ----> : $customPath"
+# if a custom installation directory  where the contents of the package has been extracted
+customPath="$(get_octopusvariable "Octopus.Action.Output.Package.InstallationDirectoryPath")"
+echo "This is not needed for the testscript ----> : $customPath"
--- scripts/PreDeploy.sh
+++ scripts/PreDeploy.sh
@@ -2,3 +2,9 @@
 ls -al
 echo "This is Predeploy test of DEV ENVIRONMENT "
 echo "This is not needed for the testscript ----> : $1"
+# in pre-deploy, in post-deploy if custom installation directory has not been defined
+extractPath="$(get_octopusvariable "Octopus.Action.Package.InstallationDirectoryPath")"
+echo "This is not needed for the testscript ----> : $extractPath"
+# if a custom installation directory has been defined
+customPath="$(get_octopusvariable "Octopus.Action.Package.CustomInstallationDirectory")"
+echo "This is not needed for the testscript ----> : $customPath"
