fe05bb93093e4a17c2fef74fc1f80fc1fff61351
sandeep kinayath <sandeepkinayath@sandeeps-MacBook-Pro.local>
2020-04-28 21:51:49 +0200

    Added changes to files

--- scripts/Deploy.sh
+++ scripts/Deploy.sh
@@ -3,5 +3,8 @@ echo "#######################################"
 echo "######          DEPLOY      ###########"
 echo "#######################################"
 extractPath="$(get_octopusvariable "Octopus.Action.Package.InstallationDirectoryPath")"
+rm -rf /home/cloud_user/Documents/app1/deployments
+ls -al
 mv /home/cloud_user/Documents/Deployment /home/cloud_user/Documents/app1/deployments
+ls -al
 echo "######  Moved files to deployments path ###########"
--- scripts/PreDeploy.sh
+++ scripts/PreDeploy.sh
@@ -7,9 +7,13 @@ echo "This is Predeploy test of PreDeploy ENVIRONMENT SCRIPT "
 # in pre-deploy, in post-deploy if custom installation directory has not been defined
 extractPath="$(get_octopusvariable "Octopus.Action.Package.InstallationDirectoryPath")"
 echo "This is extract path ---------------> : $extractPath"
+cd $extractPath
+ls -al
 # if a custom installation directory has been defined
 customPath="$(get_octopusvariable "Octopus.Action.Package.CustomInstallationDirectory")"
 echo "This is custom path -----------------> : $customPath"
+cd customPath
+ls -al
 # in pre-deploy, in post-deploy if custom installation directory has not been defined
 extractPath1="$(get_octopusvariable "Octopus.Action.Package[dev package].CustomInstallationDirectory")"
 echo "This is extract path1 ----------------> : $extractPath1"
