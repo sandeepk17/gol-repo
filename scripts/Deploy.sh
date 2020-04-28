6275c62a7924f39341d926bf277cb53db863de9e
sandeep kinayath <sandeepkinayath@sandeeps-MacBook-Pro.local>
2020-04-28 22:13:36 +0200

    Added changes to files

--- scripts/Deploy.sh
+++ scripts/Deploy.sh
@@ -4,9 +4,6 @@ echo "######          DEPLOY      ###########"
 echo "#######################################"
 extractPath="$(get_octopusvariable "Octopus.Action.Package.InstallationDirectoryPath")"
 echo "--------------------4---------------------"
-rm -rf /home/cloud_user/Documents/app1/deployments
 ls -al
 echo "-------------------moved files---------------------"
-mv /home/cloud_user/Documents/Deployment /home/cloud_user/Documents/app1/deployments
-ls -al
 echo "######  Moved files to deployments path ###########"
