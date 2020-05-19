c09c3a0b7053a9deea280ca810e91c95b2b5bcc9
Sandeep kinayath <Sandeep.kinayath@teliacompany.com>
2020-05-19 08:23:14 +0200

    Added changes to files

--- DIST/myscript.sh
+++ DIST/myscript.sh
@@ -2,10 +2,15 @@
 echo "#######################################"
 echo "######          DEPLOY      ###########"
 echo "#######################################"
+echo "-================================-"
+pwd
 extractPath="$(get_octopusvariable "Octopus.Action.Package.InstallationDirectoryPath")"
 echo "--------------------$extractPath---------------------"
 ls -al
 echo "-------------------moved files---------------------"
 echo "######  Moved files to deployments path ###########"
-echo "--------------------$1---------------------"
-echo "-------------$2"
+echo "-----------first---------$1---------------------"
+echo "---------second-----------$2---------------------"
+echo "-------devenvpackage------$3----------------------"
+cd $1
+pwd
