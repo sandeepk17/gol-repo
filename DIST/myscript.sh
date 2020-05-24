bead8bd69c16493f5f77967db4a92a457c8f6811
sandeep kinayath <sandeepkinayath@sandeeps-MacBook-Pro.local>
2020-05-24 19:45:50 +0200

    Added changes to file

--- DIST/myscript.sh
+++ DIST/myscript.sh
@@ -6,21 +6,46 @@ echo "#######################################"
 echo "-================================-"
 jboss_ctrl=$1
 env=$2
+Octopus_env="$(get_octopusvariable "Octopus.Environment.Name")"
 extractPath=`pwd`
 echo "Environment is :$env"
 echo "Deployment FOLDER is :$jboss_ctrl"
-cd $jboss_ctrl/$env
-echo "jboss drive to stop jboss"
-cd $extractPath/$env
-pwd
-echo "moving configurations and files "
-mv configuration/*.* /$jboss_ctrl/$env/configuration
-mv props/*.* /$jboss_ctrl/$env/props
-mv cert/*.* /$jboss_ctrl/$env/cert
-echo "present working directory is "
-pwd
-cd $extractPath
-if [[ ${env##*-} == "c1" ]];then
+echo "OCTOPUS ENVIRONMENT is :$Octopus_env"
+
+if [[ ${Octopus_env} == "Dev" ]];then
+    echo "DK Environment"
+    cd $jboss_ctrl/salescore-udv2-c1
+    echo "jboss drive to stop jboss"
+    rm -rf $jboss_ctrl/salescore-udv2-c1/deployments/*
+    mv distDK/*.* /$jboss_ctrl/salescore-udv2-c1/deployments
+    cd $extractPath/salescore-udv2-c1
+    echo "moving configurations and files "
+    mv configuration/*.* /$jboss_ctrl/salescore-udv2-c1/configuration
+    mv props/*.* /$jboss_ctrl/salescore-udv2-c1/props
+    mv cert/*.* /$jboss_ctrl/salescore-udv2-c1/cert
+    cd $jboss_ctrl/salescore-udv2-c1
+    echo "start jboss"
+    echo "UK Environment"
+    cd $jboss_ctrl/salescore-udv2-c2
+    echo "jboss drive to stop jboss"
+    rm -rf $jboss_ctrl/salescore-udv2-c2/deployments/*
+    mv distDK/*.* /$jboss_ctrl/salescore-udv2-c2/deployments
+    cd $extractPath/salescore-udv2-c2
+    echo "moving configurations and files "
+    mv configuration/*.* /$jboss_ctrl/salescore-udv2-c2/configuration
+    mv props/*.* /$jboss_ctrl/salescore-udv2-c2/props
+    mv cert/*.* /$jboss_ctrl/salescore-udv2-c2/cert
+    cd $jboss_ctrl/salescore-udv2-c2
+    echo "start jboss"
+elif [[ ${Octopus_env} == "Test" ]] && [[ ${env##*-} == "c1" ]];then
+    cd $jboss_ctrl/$env
+    echo "jboss drive to stop jboss"
+    cd $extractPath/$env
+    pwd
+    echo "moving configurations and files "
+    mv configuration/*.* /$jboss_ctrl/$env/configuration
+    mv props/*.* /$jboss_ctrl/$env/props
+    mv cert/*.* /$jboss_ctrl/$env/cert
     echo "DK Environment"
     echo "clean up deployments"
     rm -rf $jboss_ctrl/$env/deployments/*
@@ -33,7 +58,22 @@ if [[ ${env##*-} == "c1" ]];then
     cd $jboss_ctrl/$env/deployments
     echo "---------pwd--- contains deployment------"
     ls -al
-elif [[ ${env##*-} == "c2" ]];then
+    echo "starting Jboss"
+    cd $jboss_ctrl/$env
+    echo "jboss drive to stop jboss"
+    pwd
+elif [[ ${Octopus_env} == "Test" ]] && [[ ${env##*-} == "c2" ]];then
+    cd $jboss_ctrl/$env
+    echo "jboss drive to stop jboss"
+    cd $extractPath/$env
+    pwd
+    echo "moving configurations and files "
+    mv configuration/*.* /$jboss_ctrl/$env/configuration
+    mv props/*.* /$jboss_ctrl/$env/props
+    mv cert/*.* /$jboss_ctrl/$env/cert
+    echo "present working directory is "
+    pwd
+    cd $extractPath
     echo "UK Environment"
     echo "clean up deployments"
     rm -rf $jboss_ctrl/$env/deployments/*
@@ -46,6 +86,10 @@ elif [[ ${env##*-} == "c2" ]];then
     echo "---------pwd--- contains deployment------"
     pwd
     ls -al
+    echo "starting Jboss"
+    cd $jboss_ctrl/$env
+    echo "jboss drive to stop jboss"
+    pwd
 else
     echo "No environments are avilable for deployment"
 fi
